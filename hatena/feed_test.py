c56eb7d798bd276b322652ae9c49678e7dd7e6e0
pyohei <mukaishohei76@gmail.com>
2017-09-05 06:15:57 +0900

    Fix process entry test.

--- hatena/feed_test.py
+++ hatena/feed_test.py
@@ -8,6 +8,7 @@ class TestFeed(unittest.TestCase):
     def setUp(self):
         engine = create_engine('sqlite:///:memory:')
         self.obj = feed.Feed(engine, 'test')
+        self.response = None
 
     def test__make_feed_api_url(self):
         """Test code of _make_url"""
@@ -17,9 +18,16 @@ class TestFeed(unittest.TestCase):
     def test__request(self):
         """Test request"""
         # Same with result of `test__make_entry_api_url`
-        url = 'http://b.hatena.ne.jp/test/rss?of=test'
+        url = 'http://b.hatena.ne.jp/sample/rss?of=sample'
         r = self.obj._request(url)
         self.assertIsInstance(r, dict)
+        #self.assertEqual(r, 'aa')
+        self.response = r
+
+    def test__process_entry(self):
+        url = 'http://b.hatena.ne.jp/sample/rss?of=sample'
+        r = self.obj._request(url)
+        self.assertIsInstance(self.obj._process_entry(r), list)
 
 if __name__ == '__main__':
     unittest.main()
