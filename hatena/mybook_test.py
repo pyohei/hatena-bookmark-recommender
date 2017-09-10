58009f722b11a6e530b247835021682e82518ce1
pyohei <mukaishohei76@gmail.com>
2017-09-10 18:55:17 +0900

    Add test code

---
+++ hatena/mybook_test.py
@@ -0,0 +1,37 @@
+"""Test of mybook."""
+import unittest
+import mybook
+from sqlalchemy import create_engine
+
+class TestMyBook(unittest.TestCase):
+
+    def setUp(self):
+        engine = create_engine('sqlite:///:memory:')
+        bookmarks = ["http://test", "http://TEST"]
+        self.obj = mybook.Mybook(engine)
+        def _create_test_data():
+            """Create database for test."""
+            create_sql = """
+                CREATE TABLE `my_bookmarks` (
+                  `no` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                  `url` text,
+                  `is_search` tinyint DEFAULT 0,
+                  `invalid` tinyint DEFAULT 0,
+                  `update_time` timestamp
+                );
+                """
+            insert_sql = """
+                INSERT INTO `my_bookmarks` (url)
+                  VALUES ('http://test');
+                """
+            c = engine.connect()
+            c.execute(create_sql)
+            c.execute(insert_sql)
+        _create_test_data()
+
+    def test__has_record(self):
+        self.assertTrue(self.obj._has_record('http://test'))
+        self.assertFalse(self.obj._has_record('http://TEST'))
+
+if __name__ == '__main__':
+    unittest.main()
