f4bcbaf27d90b57d4f638ee6a5ff8758f8ae965a
pyohei <mukaishohei76@gmail.com>
2017-09-10 21:20:08 +0900

    Convert  into sqlalchemy.

--- hatena/mybook.py
+++ hatena/mybook.py
@@ -1,9 +1,15 @@
 """My Bookmark class"""
 
+from sqlalchemy import MetaData
+from sqlalchemy import Table
+from sqlalchemy.sql import select,insert
+
+
 class Mybook(object):
 
     def __init__(self, engine):
         self.engine = engine
+        self.md = MetaData(self.engine)
 
     def register(self, bookmarks):
         for b in bookmarks:
@@ -22,14 +28,8 @@ class Mybook(object):
         c.execute(sql)
 
     def _has_record(self, bookmark):
-        sql = (
-            "SELECT * "
-            "FROM my_bookmarks "
-            "WHERE url = '%s'; " % (
-                bookmark)
-            )
-        c = self.engine.connect()
-        records = c.execute(sql)
-        for r in records:
-            return True
-        return False
+        """Check bookmark url is already existing."""
+        t = Table('my_bookmarks', self.md)
+        w = "url = '{}'".format(bookmark)
+        s = select(columns=['no'], from_obj=t).where(w)
+        return s.execute().scalar()
