45b05222ff2c164dfbdc4ec565629a1a1a7f929f
pyohei <mukaishohei76@gmail.com>
2017-10-07 13:21:10 +0900

    Add Line notification.@

--- notify.py
+++ notify.py
@@ -2,10 +2,14 @@
 
 import requests
 import os
+from datetime import datetime
 
 def main():
     """Send message with Line"""
-    pass
+    params = {"value1": "{}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}
+    url = os.environ.get('HATENA_FEED_NOTIFICATION_URL', '')
+    r = requests.post(url, params)
+    print(r)
 
 if __name__ == '__main__':
     main()
