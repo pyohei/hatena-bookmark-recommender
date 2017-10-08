af0cd757df10b322e60edfc49995a71e6ba5b43b
pyohei <mukaishohei76@gmail.com>
2017-10-08 21:59:58 +0900

    Add frame.

--- notify.py
+++ notify.py
@@ -1,8 +1,20 @@
+# coding: utf-8
 """Notification recommend feed."""
 
 import requests
 import os
 from datetime import datetime
+
+class Notify(object):
+
+    def __init__(self, engine):
+        pass
+
+    def send_line(self):
+        pass
+
+    def _set_as_notified(self):
+        pass
 
 def main():
     """Send message with Line"""
