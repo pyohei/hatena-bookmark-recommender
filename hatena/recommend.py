0dbc13ecc4a888f207ff51e43277d7f494219888
pyohei <mukaishohei76@gmail.com>
2017-10-13 23:34:38 +0900

    Add notification table.

--- hatena/recommend.py
+++ hatena/recommend.py
@@ -16,7 +16,7 @@ class Recommend(object):
             rank_urls.append((rec["title"], rec["url"], rec["id"]))
         return rank_urls
 
-    def _load_top(self, num=100):
+    def _load_top(self, num=1):
         """Load top recommend url
 
         ##Sample SQL
@@ -37,11 +37,18 @@ class Recommend(object):
         my_bookmark = Table('my_bookmark', self.md, Column('url_id'))
         bookmark = Table('bookmark', self.md, Column('url_id'))
         feed = Table('feed', self.md, Column('id'), Column('title'), Column('url'))
+        notification = Table('notification', self.md, Column('url_id'))
+
         j1 = join(bookmark, feed, bookmark.c.url_id == feed.c.id)
         j2 = j1.join(my_bookmark, bookmark.c.url_id == my_bookmark.c.url_id, isouter=True)
-        s = select(columns=[feed.c.id, feed.c.url, feed.c.title]).select_from(j2).where(
-            my_bookmark.c.url_id == None).group_by(
-                bookmark.c.url_id).having(count(bookmark.c.url_id)).order_by(
-                    count(bookmark.c.url_id).desc()).limit(10)
-        #print(s)
+        j3 = j2.join(notification, notification.c.url_id == bookmark.c.url_id, isouter=True)
+
+        s = select(columns=[feed.c.id, feed.c.url, feed.c.title]).\
+                select_from(j3).where(my_bookmark.c.url_id == None).\
+                where(notification.c.url_id == None).\
+                group_by(bookmark.c.url_id).\
+                having(count(bookmark.c.url_id)).\
+                order_by(count(bookmark.c.url_id).desc()).\
+                limit(num)
+        #print(s) ### For debug
         return s.execute()
